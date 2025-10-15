52f2ae64d8e24b50be90a6d597bd63f39ed83efd
NOGUCHI HIROKI <hiroki802@hotmail.com>
2025-10-15 22:12:51 +0900

    Updates

---
+++ _app/immutable/chunks/BXitAshr.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1ice1to)==null?void 0:s.base)??"/svelte-demo";var e;const a=((e=globalThis.__sveltekit_1ice1to)==null?void 0:e.assets)??t??"";export{a,t as b};
